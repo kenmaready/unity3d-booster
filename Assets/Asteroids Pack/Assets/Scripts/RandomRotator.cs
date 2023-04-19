2bef5697b15512e48b792e771c9f5ed40338631f
kenmaready <ken@maready.com>
2023-04-19 17:42:30 -0400

    Add light rings around launch and landing pads

--- Assets/Asteroids Pack/Assets/Scripts/RandomRotator.cs
+++ Assets/Asteroids Pack/Assets/Scripts/RandomRotator.cs
@@ -1,13 +1,3 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class RandomRotator : MonoBehaviour
-{
-    [SerializeField]
-    private float tumble;
-
-    void Start()
-    {
-        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e9b22ee68610aa1b4dab8672f54b5b5f18733e5eda89bd54a6de5d7ce6b75549
+size 270
