c93b8ca13a7c4519d498f4ec767801352eca1741
kenmaready <ken@maready.com>
2023-04-21 17:40:22 -0400

    Update live/dead rocket display, make uicanvas child of persistent gamemanager

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a7ad955406b128d8e53c35b25f8e1ca0aa21df66bddb52cda8aa82c75968f78
-size 2859
+oid sha256:0a5ad1d4dee0ea6af641c3a6bceefcf0da729d50170cb65212accd3143111333
+size 2986
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4378aec9a9246bda1913be02be2d1567edff23854fd1366d15b6e12a95e98f17
-size 1314
+oid sha256:0c29c4eeb30773f423f0c28396d1d145ce1d5bc9d719c62927af82de4cb70901
+size 2062
